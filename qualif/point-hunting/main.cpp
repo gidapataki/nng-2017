8f842a957b103b6c2244d5d18fe9fde1e5a43fc8
Andras Kucsma <andras.kucsma@prezi.com>
2017-11-01 00:53:08 +0100

    Working solution for point hunting

--- qualif/point-hunting/main.cpp
+++ qualif/point-hunting/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
+using Num = unsigned long long;
+
+#if DEBUG
 #include "Cube.h"
 #include "Field.h"
 
@@ -78,22 +82,97 @@ int bruteForceMaxScore(int width, int height) {
 
     return max;
 }
+#endif
+
+Num firstRow(Num width) {
+    const int pattern[] = {6, 11, 13, 14};
+
+    --width;
+
+    return 14 * (width / 4) + pattern[width % 4];
+}
+
+Num secondRow(Num width) {
+    const int pattern[] = {11, 15, 18, 20};
+
+    --width;
+
+    return 14 * (width / 4) + pattern[width % 4];
+}
+
+Num thirdRow(Num width) {
+    const int pattern[] = {4, 7};
+
+    width -= 3;
+
+    return 18 + 7 * (width / 2) + pattern[width % 2];
+}
+
+Num fourthRow(Num width) {
+    const int pattern[] = {4, 7};
+
+    width -= 4;
+
+    return 25 + 7 * (width / 2) + pattern[width % 2];
+}
+
+Num solveConstantTime(Num width, Num height) {
+    if (height > width) {
+        std::swap(width, height);
+    }
+
+    assert(width >= height);
+
+    if (height == 1) {
+        return firstRow(width);
+    } else if (height == 2) {
+        return secondRow(width);
+    } else if (height == 3) {
+        return thirdRow(width);
+    } else if (height == 4) {
+        return fourthRow(width);
+    } else {
+        int pattern[] = {4, 7};
+        if (height % 2 == 0) {
+            pattern[0] = 3;
+        }
+
+        return fourthRow(height) + 7 * ((width - 5) / 2) + pattern[(width - 5) % 2];
+    }
+
+    return -1;
+}
 
 int main() {
+#if false && DEBUG
     int size = 15;
     std::vector<std::vector<int>> scores(15, std::vector<int>{15});
 
-    for (int y = 1; y <= 35; ++y) {
-        for (int x = 1; x <= 35; ++x) {
-            if (x + y > 25) {
-                continue;
+    for (int y = 1; y <= 11; ++y) {
+        for (int x = 1; x <= 11; ++x) {
+            auto bf = bruteForceMaxScore(x, y);
+            auto ct = solveConstantTime(x, y);
+            if (bf != ct) {
+                std::cout << "(" << bruteForceMaxScore(x, y)
+                    << ", " << solveConstantTime(x, y) << ") ";
+            } else {
+                std::cout << "(OK, OK) ";
             }
-            auto score = bruteForceMaxScore(x, y);
-            // scores[x-1][y-1] = score;
-            std::cout << score << " ";
         }
         std::cout << std::endl;
     }
+#else
+    int c;
+    std::cin >> c;
+
+    for (int i = 0; i < c; ++i) {
+        Num n, m;
+        std::cin >> n >> m;
+
+        std::cout << solveConstantTime(m, n) << std::endl;
+    }
+
+#endif
     // std::cout << bruteForceMaxScore(1, 1) << std::endl;
     // std::cout << bruteForceMaxScore(2, 1) << std::endl;
     // std::cout << bruteForceMaxScore(2, 2) << std::endl;
