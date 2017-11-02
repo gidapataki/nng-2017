14c2ee17d16197ab00a7465a667d3399ac36eba4
Matthias Vegh <matyas.vegh@gmail.com>
2017-11-02 18:36:47 +0100

    Add stub hungarian solution to sports-betting
    Hungarian library from
    RoboJackets/hungarian@bbc256dabc90c0ad39e2e548df912f4c690d7475

---
+++ qualif/sports-betting/src/hungarian_solve.cpp
@@ -0,0 +1,7 @@
+#include "solve.hpp"
+#include "hungarian.hpp"
+
+Number solve(const std::vector<std::vector<Number>>& offers,
+		const std::vector<std::size_t>& bookieLimits) {
+	return Number{};
+}
