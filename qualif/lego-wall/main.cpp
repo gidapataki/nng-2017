b577ecf002cd69f0a17702011141119f969449f9
Andras Kucsma <andras.kucsma@prezi.com>
2017-11-04 00:17:57 +0100

    Normalized lego bricks representation

--- qualif/lego-wall/main.cpp
+++ qualif/lego-wall/main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <cctype>
+#include <array>
+#include <algorithm>
+
+#include <boost/container/static_vector.hpp>
+
+using SingleBricks = boost::container::static_vector<int, 5>;
 
 struct Bricks {
-    int count = 0;
-    int length = 0;
+    std::array<SingleBricks, 4> bricks;
+
+    void Normalize();
 };
 
-std::uint64_t doTheThing(std::vector<Bricks> bricks_collection) {
+void Bricks::Normalize() {
+    for (auto& b : bricks) {
+        std::sort(b.begin(), b.end(), std::greater<int>{});
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const Bricks& bricks) {
+    for (int i = 0; i < bricks.bricks.size(); ++i) {
+        for (auto c : bricks.bricks[i]) {
+            os << (i+1) << ": " << c << ", ";
+        }
+    }
+    return os;
+}
+
+std::uint64_t doTheThing(Bricks bricks) {
+    bricks.Normalize();
+    std::cout << bricks << std::endl;
     return 0;
 }
 
@@ -18,13 +42,13 @@ int main() {
     for (int i = 0; i < test_count; ++i) {
         int height, brick_type_count;
         std::cin >> height >> brick_type_count;
-        std::vector<Bricks> bricks_collection;
+        Bricks bricks;
         for (int j = 0; j < brick_type_count; ++j) {
-            Bricks b;
-            std::cin >> b.count >> b.length;
-            bricks_collection.push_back(b);
+            int count, length;
+            std::cin >> count >> length;
+            bricks.bricks[length - 1].push_back(count);
         }
-        auto all_count = doTheThing(bricks_collection);
+        auto all_count = doTheThing(bricks);
         std::cout << all_count << std::endl;
     }
 }
