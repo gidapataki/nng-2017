531664873108066a0d8057ce9306d565b1bec4e0
Gida Pataki <gida.pataki@prezi.com>
2017-11-06 02:17:55 +0100

    Proper runtest

--- qualif/squares/runtest.sh
+++ qualif/squares/runtest.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
+if test -z "$1"
+then
+	echo "usage: $0 <executable>"
+	exit 1
+fi
+
 for infile in tests/test*.in
 do
 	echo "Testing ${infile}"
 	outfile=tests/$(basename ${infile} .in).out
-	time diff "${outfile}" <($(sugoi target -b)/squares < "${infile}")
+	diff "${outfile}" <("$1" < "${infile}")
 done
