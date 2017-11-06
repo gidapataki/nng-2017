8c098c3d889f88578d43fbfddad11e5a207c3e1b
Matthias Vegh <matyas.vegh@gmail.com>
2017-11-06 19:04:50 +0100

    Reimplement Hungarian
    Hungarian library from
    mcximing/hungarian-algorithm-cpp@0972ffae9c2729e5eb749e9c9dbb861072b6ab0e

---
+++ qualif/sports-betting/src/Hungarian.h
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////
+// Hungarian.h: Header file for Class HungarianAlgorithm.
+// 
+// This is a C++ wrapper with slight modification of a hungarian algorithm implementation by Markus Buehren.
+// The original implementation is a few mex-functions for use in MATLAB, found here:
+// http://www.mathworks.com/matlabcentral/fileexchange/6543-functions-for-the-rectangular-assignment-problem
+// 
+// Both this code and the orignal code are published under the BSD license.
+// by Cong Ma, 2016
+// 
+
+#ifndef HUNGARIAN_H
+#define HUNGARIAN_H
+
+#include <iostream>
+#include <vector>
+#include "Number.hpp"
+
+
+class HungarianAlgorithm
+{
+public:
+	HungarianAlgorithm();
+	~HungarianAlgorithm();
+	Number Solve(std::vector<std::vector<Number> >& DistMatrix, std::vector<int>& Assignment);
+
+private:
+	void assignmentoptimal(int *assignment, Number *cost, Number *distMatrix, int nOfRows, int nOfColumns);
+	void buildassignmentvector(int *assignment, bool *starMatrix, int nOfRows, int nOfColumns);
+	void computeassignmentcost(int *assignment, Number *cost, Number *distMatrix, int nOfRows);
+	void step2a(int *assignment, Number *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim);
+	void step2b(int *assignment, Number *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim);
+	void step3(int *assignment, Number *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim);
+	void step4(int *assignment, Number *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim, int row, int col);
+	void step5(int *assignment, Number *distMatrix, bool *starMatrix, bool *newStarMatrix, bool *primeMatrix, bool *coveredColumns, bool *coveredRows, int nOfRows, int nOfColumns, int minDim);
+};
+
+
+#endif
---
+++ qualif/sports-betting/src/hungarian3_solve.cpp
@@ -0,0 +1,96 @@
+#include "solve.hpp"
+#include "Hungarian.h"
+
+#include <boost/iterator/zip_iterator.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+
+namespace {
+
+std::vector<std::vector<Number>> expandMatrix(
+		const std::vector<std::vector<Number>>& offers,
+		const std::vector<std::size_t> bookieLimits) {
+	std::vector<std::vector<Number>> result;
+	result.reserve(offers.size());
+
+	for (const auto& racerOffers: offers) {
+		auto begin = boost::make_zip_iterator(
+				boost::make_tuple(racerOffers.begin(), bookieLimits.begin()));
+		auto end = boost::make_zip_iterator(
+				boost::make_tuple(racerOffers.end(), bookieLimits.end()));
+		auto range = boost::make_iterator_range(begin, end);
+
+		std::vector<Number> expandedRacerOffers;
+		for (const auto& pair: range) {
+			Number odds = pair.get<0>();
+			std::size_t multiplicity = pair.get<1>();
+			for (std::size_t i=0; i<multiplicity; ++i) {
+				expandedRacerOffers.push_back(odds);
+			}
+		}
+		result.push_back(std::move(expandedRacerOffers));
+	}
+
+	return result;
+}
+
+std::size_t totalBetsAvailable(const std::vector<std::size_t>& bookieLimits) {
+	return std::accumulate(bookieLimits.begin(), bookieLimits.end(), 0);
+}
+
+std::vector<std::vector<Number>> invert(
+		const std::vector<std::vector<Number>>& m) {
+	std::vector<std::vector<Number>> result;
+	result.reserve(m.size());
+
+	for (const auto& row: m) {
+		std::vector<Number> invertedRow;
+		invertedRow.reserve(row.size());
+		for (const auto& n: row) {
+			if (n) {
+				invertedRow.push_back(n.reciprocal());
+			} else {
+				invertedRow.push_back(Number{100000});
+			}
+		}
+		result.push_back(std::move(invertedRow));
+	}
+
+	return result;
+}
+
+} // unnamed namespace
+
+Number solve(const std::vector<std::vector<Number>>& offers,
+		const std::vector<std::size_t>& bookieLimits) {
+
+	if (totalBetsAvailable(bookieLimits) < offers.size()) {
+		// Cannot assign, not enough columns
+		return Number{0};
+	}
+
+	const auto expandedMatrix = expandMatrix(offers, bookieLimits);
+	auto invertedMatrix = invert(expandedMatrix);
+	HungarianAlgorithm solver;
+	std::vector<int> assignment;
+	auto naiveCost = solver.Solve(invertedMatrix, assignment);
+
+	std::vector<Number> chosenOdds;
+	chosenOdds.reserve(assignment.size());
+	for (std::size_t i = 0; i < assignment.size(); ++i) {
+		chosenOdds.push_back(expandedMatrix[i][assignment[i]]);
+	}
+
+	if (std::find(chosenOdds.begin(), chosenOdds.end(), Number{0})
+			!= chosenOdds.end()) {
+		return Number{0};
+	}
+
+	if (naiveCost) {
+		return naiveCost.reciprocal();
+	} else {
+		return Number{0};
+	}
+}
