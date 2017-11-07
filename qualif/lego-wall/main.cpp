1a23529a7d3369c38b2ad48ecafb172066a65c25
Andras Kucsma <andras.kucsma@prezi.com>
2017-11-07 19:49:16 +0100

    First working prototype MVP for lego

--- qualif/lego-wall/main.cpp
+++ qualif/lego-wall/main.cpp
@@ -3,6 +3,7 @@
 #include <cctype>
 #include <array>
 #include <algorithm>
+#include <utility>
 #include <unordered_map>
 
 #include <boost/functional/hash.hpp>
@@ -127,7 +128,180 @@ static std::function<std::uint64_t(Bricks, bool, bool, bool)> memoizedEveryWall4
 
 template<int H>
 std::uint64_t EveryWall4<H>::apply(Bricks bricks, bool ab, bool bc, bool cd) {
-    return 2 * memoizedEveryWall4[H-1](bricks, ab, bc, cd);
+    bricks.Normalize();
+    std::uint64_t result = 0;
+
+    // 4
+    for (int a = 0; a < bricks.bricks[4 - 1].size(); ++a) {
+        bricks.bricks[4 - 1][a] -= 1;
+        result += memoizedEveryWall4[H-1](bricks, true, true, true);
+        bricks.bricks[4 - 1][a] += 1;
+    }
+
+    // 1 - 3
+    for (int a = 0; a < bricks.bricks[1 - 1].size(); ++a) {
+        for (int b = 0; b < bricks.bricks[3 - 1].size(); ++b) {
+            bricks.bricks[1 - 1][a] -= 1;
+            bricks.bricks[3 - 1][b] -= 1;
+            result += memoizedEveryWall4[H-1](bricks, true, true, cd);
+            result += memoizedEveryWall4[H-1](bricks, ab, true, true);
+            bricks.bricks[1 - 1][a] += 1;
+            bricks.bricks[3 - 1][b] += 1;
+        }
+    }
+
+    // 2a - 2a
+    for (int a = 0; a < bricks.bricks[2 - 1].size(); ++a) {
+        if (bricks.bricks[2 - 1][a] >= 2) {
+            bricks.bricks[2 - 1][a] -= 2;
+            result += memoizedEveryWall4[H-1](bricks, true, bc, true);
+            bricks.bricks[2 - 1][a] += 2;
+        }
+    }
+
+    // 2a - 2b
+    for (int a = 0; a < bricks.bricks[2 - 1].size(); ++a) {
+        for (int b = a+1; b < bricks.bricks[2 - 1].size(); ++b) {
+            bricks.bricks[2 - 1][a] -= 1;
+            bricks.bricks[2 - 1][b] -= 1;
+            result += 2 * memoizedEveryWall4[H-1](bricks, true, bc, true);
+            bricks.bricks[2 - 1][a] += 1;
+            bricks.bricks[2 - 1][b] += 1;
+        }
+    }
+
+    // 1a - 1a - 2b
+    // 1a - 2b - 1a
+    // 2b - 1a - 1a
+    for (int a = 0; a < bricks.bricks[1 - 1].size(); ++a) {
+        for (int b = 0; b < bricks.bricks[2 - 1].size(); ++b) {
+            if (bricks.bricks[1 - 1][a] >= 2) {
+                bricks.bricks[1 - 1][a] -= 2;
+                bricks.bricks[2 - 1][b] -= 1;
+                result += memoizedEveryWall4[H-1](bricks, true, bc, cd);
+                result += memoizedEveryWall4[H-1](bricks, ab, true, cd);
+                result += memoizedEveryWall4[H-1](bricks, ab, bc, true);
+                bricks.bricks[1 - 1][a] += 2;
+                bricks.bricks[2 - 1][b] += 1;
+            }
+        }
+    }
+
+    // 1a - 1b - 2c
+    // 1b - 1a - 2c
+    // 1a - 2b - 1b
+    // 1b - 2b - 1b
+    // 2c - 1a - 1b
+    // 2c - 1b - 1a
+    for (int a = 0; a < bricks.bricks[1 - 1].size(); ++a) {
+        for (int b = a+1; b < bricks.bricks[1 - 1].size(); ++b) {
+            for (int c = 0; c < bricks.bricks[2 - 1].size(); ++c) {
+                bricks.bricks[1 - 1][a] -= 1;
+                bricks.bricks[1 - 1][b] -= 1;
+                bricks.bricks[2 - 1][c] -= 1;
+                result += 2 * memoizedEveryWall4[H-1](bricks, true, bc, cd);
+                result += 2 * memoizedEveryWall4[H-1](bricks, ab, true, cd);
+                result += 2 * memoizedEveryWall4[H-1](bricks, ab, bc, true);
+                bricks.bricks[1 - 1][a] += 1;
+                bricks.bricks[1 - 1][b] += 1;
+                bricks.bricks[2 - 1][c] += 1;
+            }
+        }
+    }
+
+    for (int a = 0; a < bricks.bricks[1 - 1].size(); ++a) {
+        bricks.bricks[1 - 1][a] -= 1;
+        for (int b = 0; b < bricks.bricks[1 - 1].size(); ++b) {
+            if (bricks.bricks[1 - 1][b] == 0) { continue; }
+            bricks.bricks[1 - 1][b] -= 1;
+            for (int c = 0; c < bricks.bricks[1 - 1].size(); ++c) {
+                if (bricks.bricks[1 - 1][c] == 0) { continue; }
+                bricks.bricks[1 - 1][c] -= 1;
+                for (int d = 0; d < bricks.bricks[1 - 1].size(); ++d) {
+                    if (bricks.bricks[1 - 1][d] == 0) { continue; }
+                    bricks.bricks[1 - 1][d] -= 1;
+                    result += memoizedEveryWall4[H-1](bricks, ab, bc, cd);
+                    bricks.bricks[1 - 1][d] += 1;
+                }
+                bricks.bricks[1 - 1][c] += 1;
+            }
+            bricks.bricks[1 - 1][b] += 1;
+        }
+        bricks.bricks[1 - 1][a] += 1;
+    }
+    return result;
+
+#if 0
+    // 1a - 1a - 1a - 1a
+    for (int a = 0; a < bricks.bricks[1 - 1].size(); ++a) {
+        if (bricks.bricks[1 - 1][a] >= 4) {
+            bricks.bricks[1 - 1][a] -= 4;
+            result += memoizedEveryWall4[H-1](bricks, ab, bc, cd);
+            bricks.bricks[1 - 1][a] += 4;
+        }
+    }
+
+    // 1a - 1a - 1a - 1b
+    // 1a - 1a - 1b - 1a
+    // 1a - 1b - 1a - 1a
+    // 1b - 1a - 1a - 1a
+    for (int a = 0; a < bricks.bricks[1 - 1].size(); ++a) {
+        for (int b = 0; b < bricks.bricks[1 - 1].size(); ++b) {
+            if (a != b && bricks.bricks[1 - 1][a] >= 3) {
+                bricks.bricks[1 - 1][a] -= 3;
+                result += 4 * memoizedEveryWall4[H-1](bricks, ab, bc, cd);
+                bricks.bricks[1 - 1][a] += 3;
+            }
+        }
+    }
+
+    return result;
+#endif
+
+#if 0
+    boost::container::static_vector<std::tuple<int, int>, 4> chosen_bricks;
+    std::vector<std::tuple<int, int>> brick_vector;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < bricks.bricks[i].size(); ++j) {
+            brick_vector.emplace_back(i, j);
+        }
+    }
+
+
+
+    for (int a = 0; a < brick_vector.size(); ++a) {
+        for (int b = 0; b < brick_vector.size(); ++b) {
+            for (int c = 0; c < brick_vector.size(); ++c) {
+                for (int d = 0; d < brick_vector.size(); ++d) {
+
+                }
+            }
+        }
+    }
+#endif
+
+#if 0
+    int length = 0;
+    while (true) {
+        for (int i = 0; i < 4; ++i) {
+            int brick_length = i+1;
+            if (length + brick_length > 4) {
+                goto done;
+            }
+            for (int j = 0; j < bricks.bricks[i].size(); ++j) {
+                int count = bricks.bricks[i][j];
+                if (count == 0) {
+                    continue;
+                }
+                length += brick_length;
+                chosen_bricks.emplace_back(i, j);
+            }
+        }
+    }
+done:
+#endif
+    return result;
+    // return 2 * memoizedEveryWall4[H-1](bricks, ab, bc, cd);
 }
 
 std::uint64_t doTheThing(int height, Bricks bricks) {
