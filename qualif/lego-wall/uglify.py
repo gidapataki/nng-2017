cfbe049f717155eaad635d70e292cabc07aa62c3
Gida Pataki <gida.pataki@prezi.com>
2017-11-09 00:29:14 +0100

    Modify uglify

--- qualif/lego-wall/uglify.py
+++ qualif/lego-wall/uglify.py
@@ -12,11 +12,16 @@ def replacement(prefix, index):
     return '{}{}'.format(prefix, index)
 
 
-def print_words(lines):
+def get_words(lines):
     words = set()
     for line in lines:
         for word in re.findall(r'\w+', line):
             words.add(word)
+    return words
+
+
+def print_words(lines):
+    words = get_words(lines)
     for w in sorted(words):
         print(w)
 
@@ -42,12 +47,46 @@ def shorten_words(fname, lines):
         sys.stdout.write(line)
 
 
+def is_protected(word):
+    if len(word) < 3:
+        return True
+
+    cc = ord(word[0])
+    if cc >= ord('0') and cc <= ord('9'):
+        return True
+
+    return False
+
+
+def protect_words(fname, lines):
+    with open(fname) as f:
+        pwords = f.readlines()
+
+    pwords = set(map(lambda s: s.strip(), pwords))
+    words = get_words(lines)
+
+    i = 0
+    repl = {}
+    for word in words:
+        if word in pwords or is_protected(word):
+            continue
+        repl[pattern(word)] = 'v{}'.format(i)
+        i += 1
+
+    for line in lines:
+        for p, s in repl.items():
+            line = re.sub(p, s, line)
+        sys.stdout.write(line)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--words', action='store_true',
                         help='print words')
     parser.add_argument('-s', '--shorten', metavar='FILE',
-                        help='word list file')
+                        help='shorten word list file')
+    parser.add_argument('-p', '--protect', metavar='FILE',
+                        help='protected words list file')
     args = parser.parse_args()
 
     lines = []
@@ -59,3 +98,6 @@ if __name__ == '__main__':
 
     if args.shorten:
         shorten_words(args.shorten, lines)
+
+    if args.protect:
+        protect_words(args.protect, lines)
