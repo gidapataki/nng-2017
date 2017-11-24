cd58afc1939ecc2bca78475336c79383e35a9f0f
Andras Kucsma <andras.kucsma@prezi.com>
2017-11-24 14:59:49 +0100

    Fix :poop:

--- finals/client/Matrix.h
+++ finals/client/Matrix.h
@@ -213,9 +213,9 @@ Matrix<T>& Matrix<T>::operator+=(const Matrix& o) {
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out, const Matrix<T>& mat) {
-	for (int row = mat.height(); row >= 0; --row) {
+	for (int row = mat.height() - 1; row >= 0; --row) {
 		for (int col = 0; col < mat.width(); ++col) {
-			out << std::setw(4) << mat(row, col) << ' ';
+			out << std::setw(5) << mat(row, col) << ' ';
 		}
 		out << std::endl;
 	}
